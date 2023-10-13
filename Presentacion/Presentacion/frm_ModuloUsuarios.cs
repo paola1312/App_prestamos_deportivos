4784ed4ed4b9c3d6f5b3454bf35d302bf3a4d215
paola1312 <mosquerapaola276@gmail.com>
2023-10-12 20:25:31 -0500

    commit 5
    Creacion Proyecto

---
+++ Presentacion/Presentacion/frm_ModuloUsuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class frm_ModuloUsuarios : Form
+    {
+        public frm_ModuloUsuarios()
+        {
+            InitializeComponent();
+        }
+    }
+}
