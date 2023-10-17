1dddd61a107d2368a3c2c58263b228ef8345ab22
paola1312 <124886092+paola1312@users.noreply.github.com>
2023-10-17 07:50:17 -0500

    commit 3.1
    Diseño formulario usuario
    Conexión Base de datos

--- Presentacion/Negocio/cls_ModulosUsuarios.cs
+++ Presentacion/Negocio/cls_ModulosUsuarios.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using MaterialSkin.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Negocio
 {
-    internal class cls_ModulosUsuarios
+    public class cls_ModulosUsuarios
     {
+        private string str_id;
+        private string str_pnombre;
+        private string str_snombre;
+        private string str_papellido;
+        private string str_sapellido;
+        private string str_contacto;
+        private string str_correo;
+        private string str_direccion;
+        private string str_sexo;
+        cls_Conexion objConectar = new cls_Conexion();
+        public void fnt_guardar (string id, string p_nombre, string s_nombre, string p_apellido, string s_apellido, string contacto,
+            string correo, string direccion, string sexo)
+        {
+            if (id.Equals("") || p_nombre.Equals("") || s_nombre.Equals("") || p_apellido.Equals("") || s_apellido.Equals("") ||
+                contacto.Equals("") || correo.Equals("") || direccion.Equals(""))
+            {
+                MessageBox.show
+            }
+        }
     }
 }
