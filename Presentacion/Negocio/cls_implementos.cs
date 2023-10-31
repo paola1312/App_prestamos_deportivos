0169086f69e87ee41be0bb0c09b61a31cb745ef8
paola1312 <124886092+paola1312@users.noreply.github.com>
2023-10-31 07:58:20 -0500

    commit
    clase 31 de octubre 2023

---
+++ Presentacion/Negocio/cls_implementos.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Negocio
+{
+   public class cls_implementos
+    {
+        private String str_codigo;
+        private String str_nombre;
+        private String str_descripcion;
+        private double flt_precio;
+        private int int_cantidad;
+        cls_Conexion objconect = new cls_Conexion();
+
+        public void fnt_registrar(string codigo, string nombre, string descripcion, double precio, int cantidad)
+        {
+            SqlCommand con = new SqlCommand("SP_AgregarImplemento", objconect.connection);
+            con.CommandType = CommandType.StoredProcedure;
+            con.Parameters.AddWithValue("@codigo", codigo);
+            con.Parameters.AddWithValue("@nombre", nombre);
+            con.Parameters.AddWithValue("@especificaciones", descripcion);
+            con.Parameters.AddWithValue("@cantidad", cantidad);
+            con.Parameters.AddWithValue("@valor", precio);
+            objconect.connection.Open();
+            con.ExecuteNonQuery();
+            objconect.connection.Close();
+            MessageBox.Show("Implemento registrado con éxito", "Registrar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public void fnt_actualizar (string codigo, string nombre, string descripcion, double precio, int cantidad)
+        {
+            SqlCommand con = new SqlCommand("SP_ActualizarImplemento", objconect.connection);
+            con.CommandType = CommandType.StoredProcedure;
+            con.Parameters.AddWithValue("@codigo", codigo);
+            con.Parameters.AddWithValue("@nombre", nombre);
+            con.Parameters.AddWithValue("@especificaciones", descripcion);
+            con.Parameters.AddWithValue("@cantidad", cantidad);
+            con.Parameters.AddWithValue("@valor", precio);
+            objconect.connection.Open();
+            con.ExecuteNonQuery();
+            objconect.connection.Close();
+            MessageBox.Show("Implemento registrado con éxito", "Registrar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public void fnt_consultar(string codigo)
+        {
+            SqlCommand con; SqlDataReader Lectura;
+            con = new SqlCommand("SP_ConsultarImplemento", objconect.connection);
+            con.CommandType = CommandType.StoredProcedure;
+            con.Parameters.AddWithValue("@codigo", codigo);
+            objconect.connection.Open();
+            Lectura = con.ExecuteReader();
+            if (Lectura.Read() == true)
+            {
+                str_nombre = Convert.ToString(Lectura[0]);
+                str_descripcion = Convert.ToString(Lectura[1]);
+                flt_precio = Convert.ToDouble(Lectura[3]);
+                int_cantidad = Convert.ToInt32(Lectura[2]);
+            }
+            objconect.connection.Close();
+        }
+        public string getNombre() { return this.str_nombre; }
+        public string getDescripcion() { return this.str_descripcion; }
+        public int getCantidad() { return this.int_cantidad; }
+        public double getValor() { return this.flt_precio; }
+    }
+}
+    
+
---
+++ Presentacion/Presentacion/frm_implementos.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Presentacion
+{
+    partial class frm_implementos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialTextBox1 = new MaterialSkin.Controls.MaterialTextBox();
+            this.SuspendLayout();
+            // 
+            // materialTextBox1
+            // 
+            this.materialTextBox1.AnimateReadOnly = false;
+            this.materialTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.materialTextBox1.Depth = 0;
+            this.materialTextBox1.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialTextBox1.Hint = "Código";
+            this.materialTextBox1.LeadingIcon = null;
+            this.materialTextBox1.Location = new System.Drawing.Point(31, 48);
+            this.materialTextBox1.MaxLength = 50;
+            this.materialTextBox1.MouseState = MaterialSkin.MouseState.OUT;
+            this.materialTextBox1.Multiline = false;
+            this.materialTextBox1.Name = "materialTextBox1";
+            this.materialTextBox1.Size = new System.Drawing.Size(138, 50);
+            this.materialTextBox1.TabIndex = 0;
+            this.materialTextBox1.Text = "";
+            this.materialTextBox1.TrailingIcon = null;
+            // 
+            // frm_implementos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.materialTextBox1);
+            this.Name = "frm_implementos";
+            this.Text = "frm_implementos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialTextBox materialTextBox1;
+    }
+}
