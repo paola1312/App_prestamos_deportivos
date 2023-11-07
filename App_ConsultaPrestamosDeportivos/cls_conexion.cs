87c618eedf11c75845fd61f41124708c05b713d3
paola1312 <124886092+paola1312@users.noreply.github.com>
2023-11-07 07:55:36 -0500

    commit-1 Proyecto Web
    Creación de formulario
    Creación clase cls_conexion
    Creacion clase cls_consulta

---
+++ App_ConsultaPrestamosDeportivos/cls_conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace App_ConsultaPrestamosDeportivos
+{
+    public class cls_conexion
+    {
+        public SqlConnection connection = new SqlConnection("Data Source=LAPTOP-E8K1E0PB\\SQLEXPRESS02;Initial Catalog=dbs_prestamos_deportivos;Integrated security = true");
+    }
+}
---
+++ App_ConsultaPrestamosDeportivos/cls_consultar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App_ConsultaPrestamosDeportivos
+{
+    public class cls_consultar
+    {
+        cls_conexion obj_conectar = new cls_conexion();
+        public void fnt_consultar(string id)
+        {
+
+        }
+    }
+}
